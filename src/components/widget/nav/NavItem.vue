be40ca6ebdc020ecbd5198e94ec634a7da6e1aae
GitHub Action <action@github.com>
2026-01-20 23:40:22 +0000

    [automated] Apply ESLint and Prettier fixes

--- src/components/widget/nav/NavItem.vue
+++ src/components/widget/nav/NavItem.vue
@@ -39,9 +39,9 @@ import { computed, ref } from 'vue'
 
 import StatusBadge from '@/components/common/StatusBadge.vue'
 import type { NavItemData } from '@/types/navTypes'
+import { cn } from '@/utils/tailwindUtil'
 
 import NavIcon from './NavIcon.vue'
-import { cn } from '@/utils/tailwindUtil'
 
 const { icon, badge, active, onClick } = defineProps<{
   icon: NavItemData['icon']
